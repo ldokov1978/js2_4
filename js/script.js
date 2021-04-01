6dfc9017ffa418bb75cf61a0b074e8ecf95cd9ff
Denis <ldokov@mail.ru>
2021-04-01 19:06:26 +0300

    Замена одинарных ковычек двойными

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 'use strict'
 window.onload = () => {
-
-
+  const text = "'I don't know,' said Martin. 'Watch a movie, maybe. Victoria said, 'I don't think so.'";
+  const sourceText = document.querySelector('.sourceText');
+  const tergetText = document.querySelector('.targetText');
+  const modifyText = text.replace(/'/gm, "\"");
+  sourceText.innerHTML = `<p>${text}</p>`;
+  tergetText.innerHTML = `<p>${modifyText}</p>`;
 };
