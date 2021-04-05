da3bd49f9240f59330e0a7d2853b783b0f1b988c
Denis <ldokov@mail.ru>
2021-04-05 08:16:08 +0300

    Обновлена строка замены

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ window.onload = () => {
   const text = "'I don't know,' said Martin. 'Watch a movie, maybe. Victoria said, 'I don't think so.'";
   const sourceText = document.querySelector('.sourceText');
   const tergetText = document.querySelector('.targetText');
-  const modifyText = text.replace(/'/gm, "\"");
+  const modifyText = text.replace(/\B[^\w\s,.]/gm, "\"");
   sourceText.innerHTML = `<p>${text}</p>`;
   tergetText.innerHTML = `<p>${modifyText}</p>`;
 };
