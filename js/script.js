6b6a7335f4fa11c7837b6f569b8b94bbdad9fbcb
Denis <ldokov@mail.ru>
2021-04-05 12:27:24 +0300

    Добавлена проверка формы

--- js/script.js
+++ js/script.js
@@ -6,4 +6,46 @@ window.onload = () => {
   const modifyText = text.replace(/\B[^\w\s,.]/gm, "\"");
   sourceText.innerHTML = `<p>${text}</p>`;
   tergetText.innerHTML = `<p>${modifyText}</p>`;
+
+  // ==================================
+
+  const mainName = document.querySelector('#name');
+  const mainPhone = document.querySelector('#phone');
+  const mainEmail = document.querySelector('#email');
+  const mainButton = document.querySelector('#mainButton');
+
+  mainButton.addEventListener('click', validMessage);
+
+  function validMessage(e) {
+    const mainNameValue = mainName.value;
+    const mainPhoneValue = mainPhone.value;
+    const mainEmailValue = mainEmail.value;
+
+    const regExpName = /[А-Яа-яA-Za-z]+/gm;
+    const regExpPhone = /\+\d\(\d{3}\)\d{3}-\d{4}/gm;
+    const regExpEmail = /[\w.-]+@[\w]+.[\w]+/gm;
+
+    if (!regExpName.test(mainNameValue)) {
+      mainName.classList.add('mainInputError');
+      alert('Введите корректное имя !');
+    } else {
+      mainName.classList.remove('mainInputError');
+    };
+
+
+    if (!regExpPhone.test(mainPhoneValue)) {
+      mainPhone.classList.add('mainInputError');
+      alert('Введите корректный номер телефона !');
+    } else {
+      mainPhone.classList.remove('mainInputError');
+    };
+
+    if (!regExpEmail.test(mainEmailValue)) {
+      mainEmail.classList.add('mainInputError');
+      alert('Введите корректный E-mail !');
+    } else {
+      mainEmail.classList.remove('mainInputError');
+    };
+    
+  }
 };
